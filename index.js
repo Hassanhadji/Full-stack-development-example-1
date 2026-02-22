bbf98693e6ba5964def1be6a5ed1f235d0f3783c
Hassan Hadji <hassan.hadji-ibrahim@outlook.fr>
2026-02-22 01:24:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Realmadrid9248!",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+
+let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
+];
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries where user_id = $1",
+    [currentUserId]
+  );
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+async function getCurrentUser() {
+  console.log("CurrentUserId:", currentUserId);
+  if (!currentUserId) return null;
+
+  const result = await db.query(
+    "SELECT * FROM users WHERE id = $1",
+    [currentUserId]
+  );
+
+  const currentUser = result.rows[0] || null;
+
+  console.log("Selected user:", currentUser);
+
+  return currentUser;
+}
+
+async function getUsers() {
+  const result = await db.query("SELECT * FROM users ORDER BY id;");
+  return result.rows;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  const users = await getUsers();
+  const currentUser = await getCurrentUser();
+
+  const safeUser = currentUser ?? users[0] ?? null;
+  const safeColor = safeUser?.color || "teal";
+
+ 
+  if (!currentUser && users.length > 0) {
+    currentUserId = users[0].id;
+  }
+
+  res.render("index.ejs", {
+    countries,
+    total: countries.length,
+    users,
+    color: safeColor,
+    currentUser: safeUser,   
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+    return res.send("No user found.");
+  }
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/user", async (req, res) => {
+if( req.body.add === "new"){
+
+  res.render("new.ejs");
+}  else {
+    currentUserId = Number( req.body.user);
+    console.log("Selected user:", currentUserId);
+
+    res.redirect("/");
+  }
+
+
+});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+
+  const name = req.body.name;
+  const color = req.body.color;
+
+  const result = await db.query(
+    "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+    [name, color]
+  );
+
+  const id = result.rows[0].id;
+  currentUserId = id;
+
+  res.redirect("/");
+});
+
+app.post("/delete", async (req, res) => {
+  try {
+    
+    const id = Number(req.body.user);
+    console.log("Deleting user id:", id);
+
+
+    await db.query(
+      "DELETE FROM users WHERE id = $1",
+      [id]
+    );
+
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting user");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
